108d48b35d5e4ccf326463979079429cb0c9f615
Alice Gaudon <alice@gaudon.pro>
2020-11-17 12:11:41 +0100

    Add temporary fix for twitter (and others) not loading in webviews
    Pending https://github.com/electron/electron/issues/25469

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import {app} from "electron";
 import Meta from "./Meta";
 import Application from "./Application";
 
+// Fix for twitter (and others) in webviews - pending https://github.com/electron/electron/issues/25469
+app.commandLine.appendSwitch('disable-features', 'CrossOriginOpenerPolicy');
+
 const application = new Application(Meta.isDevMode());
 
 // Check if application is already running
